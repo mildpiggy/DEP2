e614f98ec239a47c7a222637fce334d355925780
Feng Zhenhuan <feng_zhenhuan@gibh.ac.cb>
2022-10-14 14:58:04 +0800

    fix some warning in document

--- R/functions_app.R
+++ R/functions_app.R
@@ -1,6 +1,13 @@
+
+
+#' Run shiny application in DEP2
+#'
+#' @export
+#'
+#' @examples
 run_app <- function() {
   # Launch the app
-  appDir <- system.file("DEP2_shiny20220926", package = "DEP2")
+  appDir <- system.file("DEP2shiny20221013", package = "DEP2")
   suppressWarnings(runApp(appDir, display.mode = "normal"))
 }
 
