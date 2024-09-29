60046073b31be9139b7ed46cc6306d82d632fb76
Feng Zhenhuan <feng.zhenhuan@qq.com>
2024-09-29 18:31:43 +0800

    Highlight all chosen protein if 'onlyhighlight_Choose' in plot_volcano

--- R/plot_function_result.R
+++ R/plot_function_result.R
@@ -1317,7 +1317,7 @@ plot_volcano <- function (object, contrast = get_contrast(object)[1],
   # if
   if(onlyhighlight_Choose & length(chooseTolabel) > 0){
     df$change[which(! df$name %in% chooseTolabel)] = "stable"
-
+    df$change[which(df$name %in% chooseTolabel)] = ifelse(df[which(df$name %in% chooseTolabel)]$x > 0,'up','down')
   }
 
   name1 <- gsub("_vs_.*", "", contrast)
